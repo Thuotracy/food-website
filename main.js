f9d4b50cd79d95b1d72ea053190503ce604a006a
Thuotracy <tracyjacobs775@gmail.com>
2022-10-03 13:18:01 +0300

    made changes to the menu

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ window.addEventListener('scroll', function(){
     const header = this.document.querySelector('header');
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+let box = document.querySelector('.box');
+window.onmousemove = function(e){
+    let x = e.clientX;
+    box.style.transform = "perspective(1000px) rotateY(" + x + "deg)";
+}
